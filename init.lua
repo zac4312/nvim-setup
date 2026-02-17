dc1c9c4828cc5bad16f834bf6b60942ff0569452
zac4312 <sushi.ninja315@gmail.com>
2026-02-17 20:07:07 +0000

    final fix

--- init.lua
+++ init.lua
@@ -10,9 +10,9 @@ vim.opt.expandtab = true
 vim.opt.termguicolors = true
 
 -- Leader key
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<Space>', '', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Space>", "", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 
 -- ==========================
 -- BOOTSTRAP LAZY.NVIM
@@ -20,7 +20,9 @@ vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git", "clone", "--filter=blob:none",
+    "git",
+    "clone",
+    "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
     lazypath,
@@ -31,124 +33,135 @@ vim.opt.rtp:prepend(lazypath)
 -- ==========================
 -- PLUGINS
 -- ==========================
-
 require("lazy").setup({
 
-  -- ==========================
-  -- TREE-SITTER (Syntax Highlighting)
-  -- ==========================
+  -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
-          "lua", "javascript", "typescript", "html", "css", "python"
+          "lua", "javascript", "typescript",
+          "html", "css", "python", "sql"
         },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
+        highlight = { enable = true },
       })
     end,
   },
 
-  -- ==========================
-  -- COLORSHEME
-  -- ==========================
-vim.cmd.colorscheme("retrobox")
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
---{  "<imported colorscheme>"
-    --priority = 1000,
-    --config = function()
-      -- vim.cmd("colorscheme <colorscheme>")
-    --end, 
- -- },
-
-  -- ==========================
-  -- TELESCOPE
-  -- ==========================
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-
-  -- ==========================
-  -- FILE EXPLORER
-  -- ==========================
+  -- File Explorer
   {
     "nvim-tree/nvim-tree.lua",
     config = function()
-      require("nvim-tree").setup({
-        sort = { sorter = "case_sensitive" },
-        view = { width = 30, side = "left" },
-        renderer = { group_empty = true },
-        filters = { dotfiles = false },
-        update_focused_file = { enable = true, update_root = false },
-        sync_root_with_cwd = true,
-      })
+      require("nvim-tree").setup()
     end,
   },
 
-  -- ==========================
-  -- STATUSLINE
-  -- ==========================
-  { "nvim-lualine/lualine.nvim" },
-
-  -- ==========================
-  -- LSP + MASON
-  -- ==========================
+  -- Statusline
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
+    "nvim-lualine/lualine.nvim",
     config = function()
-      -- MASON SETUP
+      require("lualine").setup()
+    end,
+  },
+
+  -- Mason
+  {
+    "williamboman/mason.nvim",
+    config = function()
       require("mason").setup()
-      local mlsp = require("mason-lspconfig")
-      local cmp_nvim_lsp = require("cmp_nvim_lsp")
-      local capabilities = cmp_nvim_lsp.default_capabilities()
+    end,
+  },
 
-      local servers = { "lua_ls", "pyright", "tsserver" }
-      mlsp.setup({ ensure_installed = servers })
-      mlsp.setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-      })
-
-      -- KEYMAPS
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "References" })
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
-
-      -- CMP + SNIPPETS
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }),
+  -- Mason LSP bridge
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "ts_ls", 
+        },
+        automatic_installation = true,
       })
     end,
   },
+
+  -- LSP Config
+ 
+{
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+  },
+  config = function()
+
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    local servers = {
+      "lua_ls",
+      "pyright",
+      "ts_ls",
+      "sqls",
+    }
+
+    for _, server in ipairs(servers) do
+      vim.lsp.config(server, {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable(server)
+    end
+
+    -- Keymaps
+    vim.keymap.set("n", "K", vim.lsp.buf.hover)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+
+    -- Autocomplete
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      },
+    })
+
+  end,
+},
+
 
 })
+
+-- ==========================
+-- COLORSCHEME (AFTER PLUGINS)
+-- ==========================
+vim.schedule(function()
+  vim.cmd.colorscheme("retrobox")
+end)
